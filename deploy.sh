1718e7a254ee0dad04d9bcce48e3d3f694ef8a14
amaljoyc <amaljoyc@gmail.com>
2020-06-17 23:33:38 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+source env.zsh
+latest_commit=`git rev-parse HEAD | head -c 10`
+
 # npm run blog:build
 yarn build
-
-source env.zsh
 
 cd blog/.vuepress/dist
 
@@ -14,10 +15,9 @@ echo 'amaljoyc.com' > CNAME
 git init
 git add -A
 
-latest_commit=`git rev-parse HEAD | head -c 10`
 echo "deploy $latest_commit"
-git commit -m 'deploy $latest_commit'
+git commit -m "deploy $latest_commit"
 
-git push -f git@github.com:amaljoyc/amaljoyc.github.io.git master
+git push -f https://github.com/amaljoyc/amaljoyc.github.io.git master
 
 cd -
